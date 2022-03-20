81759494bded59be5b40076803f61baf71056b59
QianZuo <quinn.zuoq@gmail.com>
2022-03-20 19:31:53 +0000

    Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/api/user", (req, res) => {
+  // res.send("hello");
+  res.json({ message: "Welcome to the Support Desk API" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started on port ${PORT}`);
+});
